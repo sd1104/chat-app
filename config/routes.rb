8ecec234be785e329d92b13461baf9eb30c9ecce
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-09 15:55:35 +0900

    final_check done bofore merge

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   # resources :messages, only: :index
-  resources :groups, only: [:index,:new, :create, :edit, :update] do
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
   resources :users, only: [:edit, :update]
