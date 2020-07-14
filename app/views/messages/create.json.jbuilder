3e76e635b162d1571fe3c44d0c5859658b386ed9
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-14 19:38:55 +0900

    automatic_updating implemented

--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -2,3 +2,4 @@ json.body @message.body
 json.image @message.image.url
 json.user_name @message.user.name
 json.created_at @message.created_at.to_s(:datetime)
+json.id @message.id
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root "groups#index"
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
   resources :users, only: [:index, :edit, :update]
+  
 end
