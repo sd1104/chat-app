a04f97ff408ac719195e461e591285266b18e25e
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-14 16:57:59 +0900

    an_incremental_reserch implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.search(params[:keyword], current_user.id)
     respond_to do |format|
       format.json
+    end
   end
 
   def edit
