7031de612c416f31c50db1e5c49091887776e4f9
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-14 11:38:59 +0900

    Code to comfirm a fire of keyup made

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,12 @@
+$(function(){
+  $("#UserSearch__field").on("keyup", function(){
+    let input = $("#UserSearch__field").val();
+    console.log(input);
+    // $.ajax({
+    //   type: 'GET'
+    //   url:
+    //   data:
+    //   dataType: 'json'
+    // })
+  });
+});
