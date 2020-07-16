3356b8d522e08d0935377eba8319fe5be0e6b21e
sd1104 <67669319+sd1104@users.noreply.github.com>
2020-07-16 14:16:12 +0900

    remove unecessary comment out

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -44,7 +44,6 @@ $(function(){
   let reloadMessages = function() {
     let last_message_id = $('.Chat-main__message-list__message-box:last').data("message-id") || 0;
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
       type: 'get',
       dataType: 'json',
@@ -52,13 +51,10 @@ $(function(){
     })
     .done(function(messages) {
       if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
         let insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
-        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.Chat-main__message-list').append(insertHTML);
         $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
       }
